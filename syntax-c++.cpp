4b8a067f690ff7ec64e622155794eb7ea9c63775
Ritik Agrawal <ritikagrawal0@gmail.com>
2020-04-25 01:01:13 +0530

    Update syntax-c++.cpp

--- syntax-c++.cpp
+++ syntax-c++.cpp
@@ -3,8 +3,5 @@ Vector :
 	v.push_back (myint); v.pop_back(); //insert remove last position
 	v.erase (myvector.begin()+5); v.erase(v.begin(), v.begin()+3); //remove
 	for (auto x : v) cout << x << "\n"; //iterate
-	d	
-
-
-	
+	sort(v.begin(), v.end());
 	

59ee6b1782e07db6215661627c9f1370794b169b
Ritik Agrawal <ritikagrawal0@gmail.com>
2020-04-25 19:12:57 +0530

    Update syntax-c++.cpp

--- syntax-c++.cpp
+++ syntax-c++.cpp
@@ -1,10 +1,10 @@
 Algorithm : 
 	
 Vector :
-	vector<int> v(10, 5);// declaration size 10, initial value 5
-	v.push_back (myint); v.pop_back(); //insert remove last position
-	v.erase (myvector.begin()+5); v.erase(v.begin(), v.begin()+3); //remove
-	for (auto x : v) cout << x << "\n"; //iterate
+	vector<int> v(10, 5); // declaration size 10, initial value 5
+	v.push_back (myint); v.pop_back(); // insert remove last position
+	v.erase (myvector.begin()+5); v.erase(v.begin(), v.begin()+3); // remove
+	for (auto x : v) cout << x << "\n"; // iterate
 	sort(v.begin(), v.end()); //sort
 	find(vector.begin(), vector.end(), item)!=vector.end() // linear search
 	

dd32f8c8517e6877914ce019a024135505ce1761
Ritik Agrawal <ritikagrawal0@gmail.com>
2020-05-01 20:08:26 +0530

    Update syntax-c++.cpp

--- syntax-c++.cpp
+++ syntax-c++.cpp
@@ -5,7 +5,7 @@ Vector :
 	v.push_back (myint); v.pop_back(); // insert, remove last position
 	v.erase (myvector.begin()+5); v.erase(v.begin(), v.begin()+3); // remove
 	for (auto x : v) cout << x << "\n"; // iterate
-	sort(v.begin(), v.end()); //sort
+	sort(v, v.end()); //sort [)
 	find(vector.begin(), vector.end(), item)!=vector.end() // linear search
 		
 		

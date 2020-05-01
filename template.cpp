04c9b31746b2aad1e4d694bf182ec823c4f7cae7
Ritik Agrawal <ritikagrawal0@gmail.com>
2020-05-01 23:28:39 +0530

    Create template.cpp
    default code

---
+++ template.cpp
@@ -0,0 +1,14 @@
+#include<stdc++.h>
+
+#define for(i,b,a) for(int i = b; i <= a; ++i)
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    
+    
+    return 0;
+}

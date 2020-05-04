0a3bd722be44b259e586e6fddb13a044c8fa9f74
Ritik Agrawal <ritikagrawal0@gmail.com>
2020-05-04 12:00:19 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,12 +1,12 @@
 #include<bits/stdc++.h>
-
+#include int long long int
 #define for(i,b,a) for(int i = b; i <= a; ++i)
+#define cases(w)  int w; cin >> w; while(w--)
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
     
     return 0;
 }
